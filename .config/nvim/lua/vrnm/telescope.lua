f9b418d1ed69029914be0f16a1fc73615a9067a5
Varun Mehrishi <varunmehrishi@gmail.com>
2022-08-06 18:40:26 +0530

    Change Telescope layout config

--- .config/nvim/lua/vrnm/telescope.lua
+++ .config/nvim/lua/vrnm/telescope.lua
@@ -1,36 +1,52 @@
-require("telescope").setup {
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {
-      }
-    }
-  },
-  defaults = {
-    path_display = {"smart"}
-  }
-}
+require("telescope").setup({
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({}),
+		},
+	},
+	defaults = {
+		path_display = { "smart" },
+		layout_config = {
+			horizontal = {
+				height = 0.95,
+				preview_cutoff = 120,
+				prompt_position = "bottom",
+				width = 0.95,
+			},
+		},
+	},
+})
 
 require("telescope").load_extension("ui-select")
-require('telescope').load_extension("file_browser")
+require("telescope").load_extension("file_browser")
 -- Enable telescope fzf native, if installed
-require('telescope').load_extension("fzf")
+require("telescope").load_extension("fzf")
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>h', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>s', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 0,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer]' })
+vim.keymap.set("n", "<leader>h", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
+vim.keymap.set("n", "<leader>s", function()
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		previewer = false,
+		layout_config = {
+			center = {
+				height = 0.8,
+				width = 0.8,
+			},
+		},
+	}))
+end, { desc = "[/] Fuzzily search in current buffer]" })
 
-vim.keymap.set('n', '<leader>p', require('telescope.builtin').git_files, { desc = '[P]Git Files' })
-vim.keymap.set('n', '<leader>o', require('telescope.builtin').find_files, { desc = '[O]Search Files' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>fb', require('telescope').extensions.file_browser.file_browser, { desc = '[F]ile [B]rowser' })
-
+vim.keymap.set("n", "<leader>p", require("telescope.builtin").git_files, { desc = "[P]Git Files" })
+vim.keymap.set("n", "<leader>o", require("telescope.builtin").find_files, { desc = "[O]Search Files" })
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set(
+	"n",
+	"<leader>fb",
+	require("telescope").extensions.file_browser.file_browser,
+	{ desc = "[F]ile [B]rowser" }
+)
