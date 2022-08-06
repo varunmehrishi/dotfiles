1b63411f2d4db877953ff417be985b0ef1c73a8b
Varun Mehrishi <varunmehrishi@gmail.com>
2022-08-06 21:12:10 +0530

    Enable gitsigns code actions source in null-ls

--- .config/nvim/lua/vrnm/lsp/null-ls.lua
+++ .config/nvim/lua/vrnm/lsp/null-ls.lua
@@ -7,6 +7,7 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
@@ -18,5 +19,6 @@ null_ls.setup({
     -- formatting.clang_format,
     diagnostics.flake8,
     -- diagnostics.cppcheck,
+    code_actions.gitsigns
 	},
 })
